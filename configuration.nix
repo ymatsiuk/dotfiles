0b018142364609ff9f2f85e8b9033b4da40e4444
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2020-11-02 17:14:09 +0100

    Add NixOS config

---
+++ configuration.nix
@@ -0,0 +1,148 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      #<nixos-hardware/dell/xps/13-9370>
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_5_8;
+  networking.hostName = "nixos"; # Define your hostname.
+  time.timeZone = "Europe/Amsterdam";
+  networking.useDHCP = false;
+  networking.interfaces.enp0s3.useDHCP = true;
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
+
+  fonts.fonts = with pkgs; [
+    (nerdfonts.override {
+      fonts = [ "FiraCode" "DroidSansMono" ];
+    })
+  ];
+
+  virtualisation.docker = {
+    enable = true;
+    extraOptions = "--bip 172.18.0.1/16";
+  };
+
+  programs.sway = {
+    enable = true;
+    extraPackages = with pkgs; [
+      swaylock # lockscreen
+      swayidle
+      xwayland # for legacy apps
+      waybar # status bar
+      mako # notification daemon
+      kanshi # autorandr
+    ];
+    extraSessionCommands = ''
+      export SDL_VIDEODRIVER=wayland
+      export QT_QPA_PLATFORM=wayland
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+      export _JAVA_AWT_WM_NONREPARENTING=1
+    '';
+  };
+
+#  environment = {
+#    etc = {
+#      "sway/config".source = ./dotfiles/sway/config;
+#      "xdg/waybar/config".source = ./dotfiles/waybar/config;
+#      "xdg/waybar/style.css".source = ./dotfiles/waybar/style.css;
+#    };
+#  };
+
+  environment.systemPackages = with pkgs; [
+    wget neovim alacritty firefox dmenu starship git wl-clipboard
+    (
+      pkgs.writeTextFile {
+        name = "startsway";
+        destination = "/bin/startsway";
+        executable = true;
+        text = ''
+          #! ${pkgs.bash}/bin/bash
+
+          # first import environment variables from the login manager
+          systemctl --user import-environment
+          # then start the service
+          exec systemctl --user start sway.service
+        '';
+      }
+    )
+  ];
+
+  systemd.user.targets.sway-session = {
+    description = "Sway compositor session";
+    documentation = [ "man:systemd.special(7)" ];
+    bindsTo = [ "graphical-session.target" ];
+    wants = [ "graphical-session-pre.target" ];
+    after = [ "graphical-session-pre.target" ];
+  };
+
+  systemd.user.services.sway = {
+    description = "Sway - Wayland window manager";
+    documentation = [ "man:sway(5)" ];
+    bindsTo = [ "graphical-session.target" ];
+    wants = [ "graphical-session-pre.target" ];
+    after = [ "graphical-session-pre.target" ];
+    # We explicitly unset PATH here, as we want it to be set by
+    # systemctl --user import-environment in startsway
+    environment.PATH = lib.mkForce null;
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = ''
+        ${pkgs.dbus}/bin/dbus-run-session ${pkgs.sway}/bin/sway --debug
+      '';
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
+
+  #services.redshift = {
+  #  enable = true;
+  #  package = pkgs.redshift-wlr;
+  #};
+  #
+  services.xserver.enable = true;
+  services.xserver.displayManager.defaultSession = "sway";
+  services.xserver.displayManager.lightdm.enable = true;
+
+  programs.waybar.enable = true;
+
+  systemd.user.services.kanshi = {
+    description = "Kanshi output autoconfig ";
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    serviceConfig = {
+      # at .config/kanshi/config
+      ExecStart = ''
+        ${pkgs.kanshi}/bin/kanshi
+      '';
+      RestartSec = 5;
+      Restart = "always";
+    };
+  };
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  users.users.ymatsiuk = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "docker" ]; # Enable ‘sudo’ for the user.
+  };
+
+  system.stateVersion = "20.09"; # Did you read the comment?
+
+}
+
